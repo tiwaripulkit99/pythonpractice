c267f2a2725f88ac05704b44ea4cca59490ba64b
pulkit <tiwaripulkit99@gmail.com>
2021-04-09 12:55:56 +0530

    added

---
+++ pythonpractice/loop pr.py
@@ -0,0 +1,66 @@
+"""i=1
+while(i<=10):
+    print(i,end="   ")
+    i=i+1"""
+"""l=[]
+for i in range(0,3):
+       name=input("enter the name:")
+       l.append(name)
+print(l)
+"""
+#write a program to print the  multiplication tabel of foor loop:
+"""n=int(input("enter the number u want table:"))
+for i in range(1,10):
+    x=n*i
+    print(x)
+"""
+"""l=["pulkit","Pooja","deepa","komal"]
+for i in l:
+   if i[0]=="p":
+    print("hello",i)"""
+    #write a program to find the numbr is prime or not
+'''num=int(input("enter the number: "))
+prime=True
+for i in range(2,num):
+      if num%i==0:
+          prime=False
+          break
+if prime:
+       print("the number is prime")
+else:
+        print("the number is not prime")
+'''
+# write a program of factorial:
+"""num=int(input("enter the number: "))
+sum=1
+for i in range(1,num+1):
+    sum=sum+i
+print(sum)"""
+"""#write a program to print the star pattern:
+*
+**
+***
+****"""
+"""for i in range(5):
+    print("*"*i)
+    """
+"""write a prorgram to print pattern :  
+    *
+  * * *
+* * * * *"""
+"""for i  in range(4):
+    for j in range(6):
+      if j>=(4-i) and j<=(i+2):
+        print("* ",end=" ")
+      else:print('  ',end=" ")
+    print("\n")
+   
+        """
+"""  write a program to print the  reversed multiplication tabel of using loop:
+n=int(input("enter the number u want table:"))
+for i in range(10,0,-1):
+    x=n*i
+    print(x)"""
+"""a=1,2,3,4
+print(sum(a))
+print(a)"""
