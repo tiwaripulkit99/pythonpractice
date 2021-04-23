cd626668081c6aa9a15d1fa3ceb14b7514dd3e99
pulkit <tiwaripulkit99@gmail.com>
2021-04-23 11:33:38 +0530

    modified

--- pythonpractice/decorators.py
+++ pythonpractice/decorators.py
@@ -1,10 +1,28 @@
-#at first normal program
-"""def name(func):
-    func("hi this is unique")
-name(print) """
-#now see what is decorating
-def dec():
-    def inner():
-        return 4+5
+def div(a, b):
+    print(a / b)
+
+
+def clonediv(func):
+    def inner(a, b):
+        if a < b:
+            a, b = b, a
+        return func(a, b)
+
     return inner
-print(dec)
+
+
+div1 = clonediv(div)
+div1(2, 4)
+"""def decorator_function(passed_function):
+
+    def inner_decorator():
+        print('this happens before')
+        passed_function()
+        print('this happens after')
+    return inner_decorator
+
+@decorator_function
+def what_we_call():
+    print('The actual function we called.')
+
+what_we_call()"""
